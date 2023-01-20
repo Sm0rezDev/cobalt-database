19c388d41c9cc55942f6787d4dc8906bc138fe7e
Sm0rezdev <simonw0210@gmail.com>
2023-01-20 05:14:37 +0100

    Tests

--- tests.py
+++ tests.py
@@ -4,25 +4,30 @@ from cobalt import Cobalt
 
 sample_database = {
     'devs': [  # Table
-        {'name': 'Simon', 'email': 'simon.wei@chasacademy.se', 'role': 'Developer'},
-        {'name': 'Saeed', 'email': 'saeed.sadeghighahroodi@chasacademy.se', 'role': 'Scrum master'},
-        {'name': 'Melina', 'email': 'melina.asplund@chasacademy.se', 'role': 'Developer'},
-        {'name': 'Mary', 'email': 'mary.makseu@chasacademy.se', 'role': 'Developer'},
-        {'name': 'Jannatul', 'email': 'jannatul.ferdoese@chasacademy.se', 'role': 'Developer'},
-    ]
+        {'id': '1', 'name': 'Simon', 'email': 'simon.wei@chasacademy.se', 'role': 'Developer'},
+        {'id': '2', 'name': 'Saeed', 'email': 'saeed.sadeghighahroodi@chasacademy.se', 'role': 'Scrum master'},
+        {'id': '3', 'name': 'Melina', 'email': 'melina.asplund@chasacademy.se', 'role': 'Developer'},
+        {'id': '4', 'name': 'Mary', 'email': 'mary.makseu@chasacademy.se', 'role': 'Developer'},
+        {'id': '5', 'name': 'Jannatul', 'email': 'jannatul.ferdoese@chasacademy.se', 'role': 'Developer'},],
+    'client': [  # Table
+        {'id': '1', 'name': 'Giacomo', 'email': 'gt@sarimner.com', 'role': 'Product owner'},]
 }
+
+
+cbd = Cobalt()
+
+# Loads sample database.
+[cbd.save(key, val) for key, val in sample_database.items()]
 
 
 class TestDatabase(unittest.TestCase):
 
-    def test_select(self):
-        cbd = Cobalt()
+    def test_fetch_Wildcard(self):
+        cbd.table = 'devs'
+        self.assertEqual(cbd.fetch('', ''), cbd.data[cbd.table])
 
-        [cbd.save(key, obj) for key, obj in sample_database.items()]
+    def test_fetch_item_by_id(self):
+        self.assertEqual(cbd.fetch('1', ''), [cbd.data[cbd.table][0]])
 
-        # for key, obj in sample_database.items():
-        #     for item in obj:
-        #         self.assertEqual(cbd.select(key, item), sample_database[key][item])
-
-        results = cbd.execute('SELEC * FROM DEVS')
-        self.assertEqual(results, sample_database['devs'])
+    def test_fetch_item_by_value(self):
+        self.assertEqual(cbd.fetch('', 'name'), [val['name'] for val in cbd.data[cbd.table]])
