7e788fc6e6aa41901ef9abf41031cc984bd7b6ab
melinaasp <melina.asplund@chasacademy.se>
2023-01-22 22:14:04 +0100

    made small changes

--- retrieve_python_directory.py
+++ retrieve_python_directory.py
@@ -1,5 +1,6 @@
 import os
 
+# find python directories 
 def find_python_dirs(root_dir):
     python_dirs = []
     for dirpath, dirnames, files in os.walk(root_dir):
@@ -8,6 +9,6 @@ def find_python_dirs(root_dir):
                 python_dirs.append(os.path.join(dirpath, dirname))
     return python_dirs
 
-root_dir = '/' # or any other directory you want to start searching from
+root_dir = '/' 
 python_dirs = find_python_dirs(root_dir)
 print(python_dirs)
